b324b9ee6df8650597811146b9dadc5fc4f4eb0f
Jerome Jaglale <jerome_jaglale@sfu.ca>
2018-12-21 14:12:46 -0800

    Add sudo for Docker Compose install commands

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@
 curl -s https://get.docker.com | sudo sh
 
 # Docker Compose
-curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose --version
 
 # download images from Docker Hub, build and start containers
 ./start.sh
