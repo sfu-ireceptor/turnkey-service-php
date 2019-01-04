e8bd9c84c47ea32d17dde94f40a23384f9baa472
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-01-04 15:12:09 -0800

    Delete dataloading container when data loading is done

--- scripts/load_samples.sh
+++ scripts/load_samples.sh
@@ -11,12 +11,12 @@ export FILE_FOLDER
 
 # Notes:
 # sudo -E: make current environment variables available to the command run as root
-# docker-compose -e: these variables will be available inside the container (but not accessible in docker-compose.yml)
 # docker-compose --rm: delete container afterwards 
+# docker-compose -e: these variables will be available inside the container (but not accessible in docker-compose.yml)
 # "ireceptor-dataloading" is the service name defined in docker-compose.yml 
 # sh -c '...' is the command executed inside the container
 # $DB_HOST and $DB_DATABASE are defined in docker-compose.yml and will be substituted only when the python command is executed, INSIDE the container
-sudo -E docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service --rm run  \
+sudo -E docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service run --rm \
 			-e FILE_NAME="$FILE_NAME" \
 			-e FILE_FOLDER="$FILE_FOLDER" \
 			ireceptor-dataloading  \
