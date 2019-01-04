c25c7b2dc214b42217402e0179b28e82064e2359
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-01-03 17:25:11 -0800

    Move scripts to subfolder

---
+++ scripts/log.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service logs
---
+++ scripts/stop.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+SCRIPT_DIR=`dirname "$0"`
+sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service stop
