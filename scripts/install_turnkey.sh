94c8c969074ba0d744cc5ce6e2c9746069f4ff92
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-01-04 13:33:32 -0800

    Add script to install Docker

--- scripts/install_turnkey.sh
+++ scripts/install_turnkey.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 
-# Docker
-curl -s https://get.docker.com | sudo sh
+SCRIPT_DIR=`dirname "$0"`
 
-# Docker Compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+# install Docker
+echo "Installing Docker.."
+sudo ${SCRIPT_DIR}/install_docker.sh > /dev/null 2>&1
+echo "Done"
+echo
+
+
+# install Docker Compose
+echo "Installing Docker Compose.."
+sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose > /dev/null 2>&1
 sudo chmod +x /usr/local/bin/docker-compose
-sudo docker-compose --version
+echo "Done"
+echo
+
 
 # download images from Docker Hub, build and start containers
-SCRIPT_DIR=`dirname "$0"`
+echo "Downloading (pulling) Docker images from Docker Hub.."
 ${SCRIPT_DIR}/start_turnkey.sh
+echo "Done"
+echo
