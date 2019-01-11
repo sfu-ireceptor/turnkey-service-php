9b518c66698de5c3b21c5bf36fdf5220e6c70f73
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-01-11 14:21:32 -0800

    Make loading/dropping indexes scripts less verbose

--- scripts/drop_database_indexes.sh
+++ scripts/drop_database_indexes.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 SCRIPT_DIR=`dirname "$0"`
-sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service exec ireceptor-database mongo /app/scripts/drop_indexes.js
+sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service exec ireceptor-database mongo --quiet /app/scripts/drop_indexes.js
