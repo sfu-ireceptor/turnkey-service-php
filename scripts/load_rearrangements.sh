648e5a9596708e67953045a65e0c537df1e5edd3
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-01-10 16:11:44 -0800

    Fix bug

--- scripts/load_rearrangements.sh
+++ scripts/load_rearrangements.sh
@@ -6,7 +6,7 @@ REARRANGEMENT_TYPE="$1"
 shift
 
 while [ "$1" != "" ]; do
-	file="$1"
-	${SCRIPT_DIR}/load_one_rearrangement.sh "$file"
+	FILE="$1"
+	${SCRIPT_DIR}/load_one_rearrangement.sh "$REARRANGEMENT_TYPE" "$FILE"
 	shift
 done
