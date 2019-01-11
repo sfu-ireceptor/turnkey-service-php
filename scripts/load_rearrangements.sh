da7ab274a251cbb52b6d2ffdece18ee117ce39f6
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-01-11 14:30:08 -0800

    Add drop/create indexes to rearrangements loading script

--- scripts/load_rearrangements.sh
+++ scripts/load_rearrangements.sh
@@ -5,8 +5,12 @@ SCRIPT_DIR=`dirname "$0"`
 REARRANGEMENT_TYPE="$1"
 shift
 
+${SCRIPT_DIR}/drop_database_indexes.sh
+
 while [ "$1" != "" ]; do
 	FILE="$1"
 	${SCRIPT_DIR}/load_one_rearrangement.sh "$REARRANGEMENT_TYPE" "$FILE"
 	shift
 done
+
+${SCRIPT_DIR}/create_database_indexes.sh
