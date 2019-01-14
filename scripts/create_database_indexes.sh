0f8d006aca70bfb2f59eb1e9b3b7689705626c98
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-01-14 13:41:55 -0800

    Update index creation/drop scripts to match new location in database container

--- scripts/create_database_indexes.sh
+++ scripts/create_database_indexes.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 SCRIPT_DIR=`dirname "$0"`
-sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service exec ireceptor-database mongo --quiet /app/01_create_indexes.js
+sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service exec ireceptor-database mongo --quiet /app/scripts/create_indexes.js
