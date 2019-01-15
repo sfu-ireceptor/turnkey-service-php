605d68d0440a811701bf6a7770f5ed84070cc6c7
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-01-15 15:39:39 -0800

    Update script to create db indexes

--- scripts/create_database_indexes.sh
+++ scripts/create_database_indexes.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 SCRIPT_DIR=`dirname "$0"`
-sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service exec ireceptor-database mongo --quiet /app/scripts/create_indexes.js
+
+# create indexes
+sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service exec ireceptor-database \
+		sh -c 'cd /app && mongo --quiet /app/scripts/create_indexes.js'
+
+# create query plans
+sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service exec ireceptor-database \
+		sh -c 'cd /app && mongo --quiet /app/scripts/create_query_plans.js'
