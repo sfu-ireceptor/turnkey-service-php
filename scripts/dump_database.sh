e2119146171fb3513a4e7073dba6999470e03f7c
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-02-04 12:29:05 -0800

    Add scripts to backup/restore database

---
+++ scripts/dump_database.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+SCRIPT_DIR=`dirname "$0"`
+
+sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service exec -T ireceptor-database \
+	sh -c 'mongodump --archive'
