d7aaa8e88a75aa5cf8b5feafb4b5aa787a19b3ea
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-02-06 13:12:34 -0800

    Update script to connect to mongodb

--- scripts/mongo.sh
+++ scripts/mongo.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR=`dirname "$0"`
 
-sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service exec ireceptor-database mongo
+sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service exec ireceptor-database sh -c 'mongo $MONGO_INITDB_DATABASE'
