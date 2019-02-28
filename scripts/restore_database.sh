0a958c7a9b8ac91f519f81b6825d6c7b72f9cf5d
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-02-28 14:18:22 -0800

    Update db restore script to drop indexes before restore and not build them during

--- scripts/restore_database.sh
+++ scripts/restore_database.sh
@@ -2,5 +2,9 @@
 
 SCRIPT_DIR=`dirname "$0"`
 
+${SCRIPT_DIR}/drop_database_indexes.sh
+
 sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service exec -T ireceptor-database \
-	sh -c 'mongorestore --archive'
+	sh -c 'mongorestore --noIndexRestore --archive'
+
+${SCRIPT_DIR}/create_database_indexes.sh
