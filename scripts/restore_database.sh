aaffa2535646e931f7480a41364a83c97dff5c17
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-02-28 14:41:09 -0800

    Improve feedback of database restore script

--- scripts/restore_database.sh
+++ scripts/restore_database.sh
@@ -6,5 +6,5 @@ sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnk
 	sh -c 'mongo --quiet /app/scripts/drop_indexes.js && \
 			mongorestore --noIndexRestore --archive && \
 			cd /app && mongo --quiet /app/scripts/create_indexes.js && \
-			cd /app && mongo --quiet /app/scripts/create_query_plans.js \
+			cd /app && mongo --quiet /app/scripts/create_query_plans.js && \
 			echo "Your database was sucessfully restored."'
