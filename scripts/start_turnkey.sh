3b97ad0c2eb25a8b3cd130b9658891dffaa43580
Brian Corrie <bdcorrie@gmail.com>
2019-03-06 22:25:47 +0000

    Added the creation of the query plans to the process of starting the Mongo repopistory. Query plans are not persistent across mongo server restarts and therefore they needed to be re-created each time the server starts.

--- scripts/start_turnkey.sh
+++ scripts/start_turnkey.sh
@@ -2,3 +2,7 @@
 
 SCRIPT_DIR=`dirname "$0"`
 sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service up -d
+
+# The Mongo query plans are forgotten each time mongo is stopped.
+# They need to be recreated at startup.
+${SCRIPT_DIR}/create_database_queryplans.sh
