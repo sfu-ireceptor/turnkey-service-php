f3c8f361793c738d4f13489af7b4d8227bfcd74c
Brian Corrie <bdcorrie@gmail.com>
2019-03-07 21:29:19 +0000

    Simplified the command, removed the complexity of having to specify the DB information, as it is provided in the docker compose file.

--- scripts/test_performance.sh
+++ scripts/test_performance.sh
@@ -7,23 +7,14 @@ count=3
 
 # Get the command line arguements, the number of iterations, the DB name,
 # the DB host, and the DB port to use.
-if [ $# -eq 5 ]
+if [ $# -eq 1 ]
 then
     count=$1
-    db_name=$2
-    db_host=$3
-    db_port=$4
-    out_folder=$5
 else
-    echo "usage: $0 count db host port out_folder"
+    echo "usage: $0 count"
     exit
 fi
 
-# The Javascript files to use for the performance test.
-APP_DIR="/app/benchmark"
-perf_js_file="$APP_DIR/test_performance_explain.js"
-cache_js_file="$APP_DIR/cache_dump.js"
-index_js_file="$APP_DIR/index_dump.js"
 # The host we are running on
 host_name=$(hostname)
 
@@ -35,15 +26,15 @@ echo "Test performed at: $current_time"
 # is not as good as expected this can help diagnose the problem.
 sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml \
         --project-name turnkey-service run ireceptor-performance-testing \
-        sh -c "mongo $db_name --host $db_host --port $db_port $index_js_file" \
-        > $out_folder/index-$host_name-$db_host-$db_port-$db_name-$current_time.txt
+        sh -c 'mongo $DB_DATABASE --host $DB_HOST /app/benchmark/index_dump.js' \
+        > index-$host_name-$current_time.txt
 
 # Dump the query plan cache. This is important to know as if the performance
 # is not as good as expected this can help diagnose the problem.
 sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml \
         --project-name turnkey-service run ireceptor-performance-testing \
-        sh -c "mongo $db_name --host $db_host --port $db_port $cache_js_file" \
-        > $out_folder/cache-$host_name-$db_host-$db_port-$db_name-$current_time.txt
+        sh -c 'mongo $DB_DATABASE --host $DB_HOST /app/benchmark/cache_dump.js' \
+        > cache-$host_name-$current_time.txt
 
 # Perform the benchmark test the number of times requested.
 for i in `seq 1 $count`;
@@ -53,8 +44,8 @@ do
     echo "Performing test iteration $i"
     sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml \
         --project-name turnkey-service run ireceptor-performance-testing \
-        sh -c "mongo $db_name --host $db_host --port $db_port $perf_js_file" \
-        > $out_folder/run$i-$host_name-$db_host-$db_port-$db_name-$current_time.txt
+        sh -c 'mongo $DB_DATABASE --host $DB_HOST /app/benchmark/test_performance_explain.js' \
+        > run$i-$host_name-$current_time.txt
 done
 
 # Print out the time when the test run finished.
