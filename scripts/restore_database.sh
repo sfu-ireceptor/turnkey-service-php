092b36a7c46de3b2b48df7b0f550ad73d9e9ec01
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-03-28 14:58:22 -0700

    minor

--- scripts/restore_database.sh
+++ scripts/restore_database.sh
@@ -5,7 +5,9 @@ SCRIPT_DIR=`dirname "$0"`
 sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service exec -T ireceptor-database \
 	sh -c 'mongo --quiet /app/scripts/drop_indexes.js && \
 			echo && \
+			echo "Restoring database..." \
 			mongorestore --noIndexRestore --archive && \
+			echo "Done" \
 			echo && \
 			cd /app && mongo --quiet /app/scripts/create_indexes.js && \
 			cd /app && mongo --quiet /app/scripts/create_query_plans.js && \
