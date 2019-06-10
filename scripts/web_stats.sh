74409a82a58a9653035a828e6df102adcfd6e364
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-06-10 13:14:32 -0700

    Update script to generate web statistics

--- scripts/web_stats.sh
+++ scripts/web_stats.sh
@@ -2,12 +2,39 @@
 
 SCRIPT_DIR=`dirname "$0"`
 APACHE_LOG_FOLDER="${SCRIPT_DIR}/../.apache_log"
+AWSTATS_FOLDER="${SCRIPT_DIR}/../.awstats"
 
 mkdir -p "${APACHE_LOG_FOLDER}"
+mkdir -p "${AWSTATS_FOLDER}"
 
-echo "Dumping Apache logs"
-sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service logs ireceptor-api > "${APACHE_LOG_FOLDER}/access.log"
-
+echo "Delete any existing AWStats container..."
+sudo docker stop awstats && sudo docker rm awstats
 echo "Done"
 echo
 
+echo "Dumping iReceptor Turnkey API container log into ${APACHE_LOG_FOLDER}..."
+sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service logs ireceptor-api | cut -f2 -d'|' > "${APACHE_LOG_FOLDER}/access.log"
+echo "Done"
+echo
+
+echo "Starting AWStats container..."
+sudo docker run \
+    --detach \
+    --restart always \
+    --publish 8088:80 \
+    --name awstats \
+    --env AWSTATS_CONF_LOGFORMAT=' %host %other %logname %time1 %methodurl %code %bytesd %refererquot %uaquot' \
+    --volume "${APACHE_LOG_FOLDER}":/var/local/log:ro \
+    --volume "${AWSTATS_FOLDER}":/var/lib/awstats \
+    pabra/awstats
+echo "Done"
+echo
+
+echo "Running AWStats..."
+sudo docker exec awstats awstats_updateall.pl now
+echo "Done"
+echo
+
+# confirm success
+echo "Your web statistics are now available at http://localhost:8088"
+echo 'Note: you may need to replace "localhost" by this machine hostname'
