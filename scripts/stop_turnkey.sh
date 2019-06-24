92d151f33a899fcb38073c78698df1e2a45ae2ff
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-06-24 12:49:56 -0700

    Stop web stats in stop turnkey script

--- scripts/stop_turnkey.sh
+++ scripts/stop_turnkey.sh
@@ -2,3 +2,9 @@
 
 SCRIPT_DIR=`dirname "$0"`
 sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service stop
+
+echo
+echo "Stopping AWStats..."
+sudo docker stop awstats > /dev/null 2>&1 && sudo docker rm awstats > /dev/null 2>&1
+echo "Done"
+echo
