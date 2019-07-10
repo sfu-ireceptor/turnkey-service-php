1b94c93bc8c90905b7f1d2579cfd8f6ab53da72b
Laura Gutierrez Funderburk <lfunderburk@users.noreply.github.com>
2019-07-10 11:14:51 -0700

    add variable to uniquely identify samples

--- scripts/verify_dataload.sh
+++ scripts/verify_dataload.sh
@@ -3,14 +3,15 @@
 SCRIPT_DIR=`dirname "$0"`
 
 # Get the command line arguements
-if [ $# -eq 4 ]
+if [ $# -eq 5 ]
 then
     METADATA_FILE="$1"
-    API_FILE="$2"
+    API_URL="$2"
     STUDY_ID="$3"
     ANNOTATION_DIR="$4"
+    UNIQUE_ID_FIELD="$5"
 else
-    echo "usage: $0 metadata_file api_file study_id annotation_dir"
+    echo "usage: $0 metadata_file API_url_address study_id annotation_dir unique_identifier"
     exit
 fi
 
@@ -18,9 +19,10 @@ fi
 
 # make available to docker-compose.yml
 export METADATA_FILE
-export API_FILE
+export API_URL
 export STUDY_ID
 export ANNOTATION_DIR
+export UNIQUE_ID_FIELD
 
 # create log file
 LOG_FOLDER=${SCRIPT_DIR}/../log
@@ -36,15 +38,18 @@ LOG_FILE=${LOG_FOLDER}/${TIME1}_${FILE_NAME}.log
 # sh -c '...' is the command executed inside the container
 sudo -E docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service run -v /data:/data --rm \
 			-e METADATA_FILE="$METADATA_FILE" \
-			-e API_FILE="$API_FILE" \
+			-e API_URL="$API_URL" \
 			-e STUDY_ID="$STUDY_ID" \
 			-e ANNOTATION_DIR="$ANNOTATION_DIR" \
+			-e UNIQUE_ID_FIELD="$UNIQUE_ID_FIELD" \
 			ireceptor-dataloading \
 				sh -c 'python /app/verify/sanitychecking.py \
                                         $METADATA_FILE \
-                                        $API_FILE \
+                                        $API_URL \
                                         $STUDY_ID \
                                         $ANNOTATION_DIR \
-                                        L' \
+					$API_URL \
+                                        L \
+					$UNIQUE_ID_FIELD '\
  	2>&1 | tee $LOG_FILE
 
