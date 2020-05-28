c2e11cb1d948eeaaec32b704b9a63e0d814ea936
Laura Gutierrez Funderburk <lfunderburk@users.noreply.github.com>
2020-05-28 10:15:56 -0700

    modify sanity check script parameters, script name

--- scripts/verify_dataload.sh
+++ scripts/verify_dataload.sh
@@ -3,13 +3,18 @@
 SCRIPT_DIR=`dirname "$0"`
 
 # Get the command line arguements
-if [ $# -eq 5 ]
+if [ $# -eq 10 ]
 then
-    METADATA_FILE="$1"
-    API_URL="$2"
-    STUDY_ID="$3"
-    ANNOTATION_DIR="$4"
-    UNIQUE_ID_FIELD="$5"
+    mapping_file="$1"
+    base_url="$2"
+    entry_point="$3"
+    json_files="$4"
+    master_md="$5"
+    study_id="$6"
+    facet_count="$7"
+    annotation_dir="$8"
+    details_dir="$9"
+    Coverage="$10"
 else
     echo "usage: $0 metadata_file API_url_address study_id annotation_dir unique_identifier"
     exit
@@ -18,11 +23,16 @@ fi
 
 
 # make available to docker-compose.yml
-export METADATA_FILE
-export API_URL
-export STUDY_ID
-export ANNOTATION_DIR
-export UNIQUE_ID_FIELD
+export mapping_file
+export base_url
+export entry_point
+export json_files
+export master_md
+export study_id
+export facet_count
+export annotation_dir
+export details_dir
+export Coverage 
 
 # create log file
 LOG_FOLDER=${SCRIPT_DIR}/../log
@@ -37,19 +47,27 @@ LOG_FILE=${LOG_FOLDER}/${TIME1}_${FILE_NAME}.log
 # "ireceptor-dataloading" is the service name defined in docker-compose.yml 
 # sh -c '...' is the command executed inside the container
 sudo -E docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service run -v /data:/data --rm \
-			-e METADATA_FILE="$METADATA_FILE" \
-			-e API_URL="$API_URL" \
-			-e STUDY_ID="$STUDY_ID" \
-			-e ANNOTATION_DIR="$ANNOTATION_DIR" \
-			-e UNIQUE_ID_FIELD="$UNIQUE_ID_FIELD" \
+			-e mapping_file="$mapping_file" \
+			-e base_url="$base_url" \
+			-e entry_point="$entry_point" \
+			-e json_files="$json_files" \
+			-e master_md="$master_md" \
+			-e study_id="$study_id" \
+			-e facet_count="$facet_count" \
+			-e annotation_dir="$annotation_dir" \
+			-e details_dir="$details_dir" \
+			-e Coverage="$Coverage" \
 			ireceptor-dataloading \
-				sh -c 'python /app/verify/sanitychecking.py \
-                                        $METADATA_FILE \
-                                        $API_URL \
-                                        $STUDY_ID \
-                                        $ANNOTATION_DIR \
-                    					$API_URL \
-                                        L \
-					$UNIQUE_ID_FIELD '\
+				sh -c 'python /app/verify/AIRR-repertoire-checks.py \
+                                        $mapping_file \
+                                        $base_url \
+                                        $entry_point \
+                                        $json_files \
+                    			$master_md \
+					$study_id \
+					$facet_count \
+                                        $annotation_dir \
+					$details_dir \
+					$CC-FC '\
  	2>&1 | tee $LOG_FILE
 
