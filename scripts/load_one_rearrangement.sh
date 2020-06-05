b97108756f528b0bd98828c3cdad833119ddd296
Ubuntu <ubuntu@covid19-3.arbutus>
2020-06-05 18:55:24 +0000

    Added in the -v again...

--- scripts/load_one_rearrangement.sh
+++ scripts/load_one_rearrangement.sh
@@ -29,7 +29,7 @@ sudo -E docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name tu
 			-e FILE_FOLDER="$FILE_FOLDER" \
 			-e REARRANGEMENT_TYPE="$REARRANGEMENT_TYPE" \
 			ireceptor-dataloading \
-				sh -c 'python /app/dataload/dataloader.py \
+				sh -c 'python /app/dataload/dataloader.py -v\
 					--mapfile=/app/config/AIRR-iReceptorMapping.txt \
 					--host=$DB_HOST \
 					--database=$DB_DATABASE \
