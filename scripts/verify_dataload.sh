a7be28ca68b06c7f2fd57245616b14d7970edca0
Laura Gutierrez Funderburk <lfunderburk@users.noreply.github.com>
2020-07-23 20:13:39 -0700

    substitute for bash script

--- scripts/verify_dataload.sh
+++ scripts/verify_dataload.sh
@@ -19,6 +19,7 @@ export base_url
 export master_md
 export annotation_dir
 export study_id
+# Not sure if I need to export variables below? 
 export mapping_file
 export json_files
 export facet_count
@@ -33,41 +34,20 @@ TIME1=`date +%Y-%m-%d_%H-%M-%S`
 LOG_FILE=${LOG_FOLDER}/${TIME1}_${FILE_NAME}.log
 
 # -----------------------------------------------------------------------------------#
-# Sanity check for mapping and AIRR library version
+# Sanity check for mapping and AIRR library version - this changes with time though, might be worth to remove echo message, or update script accordingly
 echo "Mapping file from branch ipa5-v3 https://github.com/sfu-ireceptor/config"
 echo "AIRR test version Tag v1.3.0"
 
 # -----------------------------------------------------------------------------------#
+# INTERNAL VARIABLES 
 # Mapping file
-echo "Getting mapping file"
-git clone https://github.com/sfu-ireceptor/config
-cd config/
-git pull
-git checkout ipa5-v3
-# Get mapping file
-mapping_file=${PWD}"/AIRR-iReceptorMapping.txt"
-
-cd ${SCRIPT_DIR}
-
-# -----------------------------------------------------------------------------------#
+mapping_file="/app/config/AIRR-iReceptorMapping.txt"
 # JSON input handling
-echo "Getting JSON main directory"
-git clone https://github.com/sfu-ireceptor/dataloading-mongo
-cd dataloading-mongo/verify/
+no_filters="/app/verify/nofilters.json"
 # Get No filters query
-no_filters=${PWD}"/nofilters.json"
-cd facet_queries_for_sanity_tests/
-mkdir study_id/
-cd study_id/
-# Get Path to JSON files (facet queries)
-json_facet=$PWD"/"
-
-cd ${SCRIPT_DIR}
-
-# -----------------------------------------------------------------------------------#
-# Misc variables 
+json_facet="/app/facet_queries_for_sanity_tests/"
 entry_point="repertoire"
-details_dir=${SCRIPT_DIR}
+details_dir=$PWD
 
 # -----------------------------------------------------------------------------------#
 # Generate JSON
@@ -77,43 +57,27 @@ details_dir=${SCRIPT_DIR}
 # docker-compose -e: these variables will be available inside the container (but not accessible in docker-compose.yml)
 # "ireceptor-dataloading" is the service name defined in docker-compose.yml 
 # sh -c '...' is the command executed inside the container
-echo "Generating JSON input files"
+echo "Generating JSON input files - then running ADC API repertoire test"
 sudo -E docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service run -v /data:/data --rm \
 			-e base_url="$base_url" \
 			-e entry_point="$entry_point" \
-			-e json_files="$no_filters" \
-			ireceptor-dataloading \
-				sh -c 'python /app/verify/generate_facet_json.py \
-                                        $base_url \
-                                        $entry_point \
-                                        $no_filters \
- 	2>&1 | tee $LOG_FILE
-
-cd ${SCRIPT_DIR}
-
-echo "Verifying data at the repertoire level"
-
-sudo -E docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service run -v /data:/data --rm \
-			-e mapping_file="$mapping_file" \
-			-e base_url="$base_url" \
-			-e entry_point="$entry_point" \
-			-e json_files="$no_filters" \
-			-e master_md="$master_md" \
+			-e json_facet="$json_facet" \
+			-e no_filters="$no_filters" \
 			-e study_id="$study_id" \
-			-e facet_count="$json_facet" \
-			-e annotation_dir="$annotation_dir" \
-			-e details_dir="$details_dir" \
-			-e Coverage="$Coverage" \
+			-e mapping_file="$mapping_file" \
+			-e master_md="$master_md" \ 
+			-e annotation_dir="$annotation_dir"\
+			-e details_dir="$details_dir"
 			ireceptor-dataloading \
-				sh -c 'python /app/verify/AIRR-repertoire-checks.py \
-                                        $mapping_file \
+				sh -c 'bash /app/verify/joint_sanity_testing.sh \
                                         $base_url \
                                         $entry_point \
-                                        $no_filters \
-                    			$master_md \
+                                        /app/facet_queries_for_sanity_tests/ \ 
+					/app/verify/nofilters.json \
 					$study_id \
-					$json_facet \
-                                        $annotation_dir \
+					/app/config/AIRR-iReceptorMapping.txt \
+					$master_md \
+					$annotation_dir \
 					$details_dir \
-					$CC-FC '\
+					
  	2>&1 | tee $LOG_FILE
