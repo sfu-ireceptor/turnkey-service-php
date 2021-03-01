343ced893d3b213fe324f0c1e8fff391faf143fd
Ubuntu <ubuntu@turnkey-test2.arbutus>
2021-03-01 20:28:30 +0000

    Version working with mounts for directories required.

--- scripts/verify_dataload.sh
+++ scripts/verify_dataload.sh
@@ -3,27 +3,36 @@
 SCRIPT_DIR=`dirname "$0"`
 
 # Get the command line arguements
-if [ $# -eq 4 ]
+if [ $# -eq 5 ]
 then
     base_url="$1"
-    master_md="$2"
-    annotation_dir="$3"
-    study_id="$4" 
+    metadata="$2"
+    study_id="$3" 
+    study_dir="$4"
+    output_dir="$5"
 else
-    echo "usage: $0 base_url master_md annotation_dir study_id"
+    echo "usage: $0 base_url metadata study_id study_dir output_dir"
     exit
 fi
 
 # make available to docker-compose.yml
 export base_url
-export master_md
-export annotation_dir
+export metadata
+export study_dir
 export study_id
 export entry_point
 export json_facet
 export no_filters
 export mapping_file
-export details_dir
+export output_dir
+
+# Get the filename of metadata file. Note this container mounts the 
+# FILE_FOLDER directory as /scratch, and anything that is in $FILE_FOLDER
+# will be available on /scratch within the container. This means that the
+# FILE_NAME is available as /scratch/$FILE_NAME inside the container.
+FILE_ABSOLUTE_PATH=`realpath "$metadata"`
+FILE_FOLDER=`dirname "$FILE_ABSOLUTE_PATH"`
+FILE_NAME=`basename "$FILE_ABSOLUTE_PATH"`
 
 # create log file
 LOG_FOLDER=${SCRIPT_DIR}/../log
@@ -44,25 +53,32 @@ echo "AIRR test version Tag v1.3.0"
 # "ireceptor-dataloading" is the service name defined in docker-compose.yml 
 # sh -c '...' is the command executed inside the container
 echo "Generating JSON input files - then running ADC API repertoire test"
-sudo -E docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service run -v /data:/data --rm \
+echo $output_dir
+echo $study_dir
+echo $FILE_FOLDER
+echo $FILE_NAME
+echo $metadata
+sudo -E docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service run -v $study_dir:/study -v $output_dir:/output --rm \
 			-e base_url="$base_url" \
 			-e entry_point="$entry_point" \
 			-e json_facet="$json_facet" \
 			-e no_filters="$no_filters" \
 			-e study_id="$study_id" \
 			-e mapping_file="$mapping_file" \
-			-e master_md="$master_md" \
-			-e annotation_dir="$annotation_dir"\
+			-e metadata="$metadata" \
+			-e study_dir="$study_dir"\
 			-e details_dir="$details_dir" \
+                        -e FILE_FOLDER="$FILE_FOLDER" \
+                        -e FILE_NAME="$FILE_NAME" \
 			ireceptor-dataloading \
-				sh -c 'bash /app/verify/joint_sanity_testing.sh \
+				sh -c 'ls /scratch ; file /scratch/${FILE_NAME} ; bash /app/verify/joint_sanity_testing.sh \
                                         ${base_url} \
                                         "repertoire" \
                                         /app/verify/facet_queries_for_sanity_tests/ \
 					/app/verify/nofilters.json \
 					${study_id} \
 					/app/config/AIRR-iReceptorMapping.txt \
-					${master_md} \
-					${annotation_dir} \
-					${LOG_FOLDER}' \
+					/study/${FILE_NAME} \
+					/study/ \
+					/output/ ' \
  	2>&1 | tee $LOG_FILE
