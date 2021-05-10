c75ba1449d2be4914a38d6bc170f0f4d29c8c959
Brian Corrie <bdcorrie@gmail.com>
2021-05-10 19:58:51 +0000

    Removed commented out lines from script.

--- scripts/load_rearrangements_with_indexes.sh
+++ scripts/load_rearrangements_with_indexes.sh
@@ -17,15 +17,12 @@ date
 REARRANGEMENT_TYPE="$1"
 shift
 
-## ${SCRIPT_DIR}/drop_rearrangement_database_indexes_for_dataloading.sh
-
+# Process each file provided
 while [ "$1" != "" ]; do
 	FILE="$1"
 	${SCRIPT_DIR}/load_one_rearrangement.sh "$REARRANGEMENT_TYPE" "$FILE"
 	shift
 done
-
-## ${SCRIPT_DIR}/create_rearrangement_database_indexes.sh
 
 echo -n "Done $0: "
 date
