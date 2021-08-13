d69306c812b0537f16178785796db35dc50117c3
bcorrie <bdcorrie@gmail.com>
2021-08-13 21:57:02 +0000

    Changed it so the master branch is pulling the master config AIRR Mapping.

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,1 +1,1 @@
-MAPPING_URL=https://raw.githubusercontent.com/sfu-ireceptor/config/clone-and-stats-mapping/AIRR-iReceptorMapping.txt
+MAPPING_URL=https://raw.githubusercontent.com/sfu-ireceptor/config/master/AIRR-iReceptorMapping.txt
