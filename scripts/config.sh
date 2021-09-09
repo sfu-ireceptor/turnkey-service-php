df1b3f87d8370bf02b8ee9d7c4be8ad83cf5f846
Dinis Trigo <dinis.f.trigo@inesctec.pt>
2021-09-09 17:54:36 +0100

    changed config.sh to point to the reproducibility branch

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,1 +1,1 @@
-MAPPING_URL=https://raw.githubusercontent.com/sfu-ireceptor/config/master/AIRR-iReceptorMapping.txt
+MAPPING_URL=https://raw.githubusercontent.com/sfu-ireceptor/config/reproducibility/AIRR-iReceptorMapping.txt
