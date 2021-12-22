4d01d6832aad1a62e6ce537aed9080dbf76253e1
jeromejaglale <jerome.jaglale@gmail.com>
2021-12-22 23:45:19 +0000

    Rename Apache vhosts

--- scripts/enable_https.sh
+++ scripts/enable_https.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=`dirname "$0"`
 # install Docker
 echo "Enabling HTTPS.."
 sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service exec -T ireceptor-api \
-		sh -c 'a2dissite -q default-ssl.conf && a2ensite -q 000-default.conf && service apache2 reload'
+		sh -c 'a2dissite -q http.conf && a2ensite -q 000-default.conf && service apache2 reload'
 echo "Done"
 echo
 
