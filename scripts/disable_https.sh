6c84b8d6e8a7439e22a96bd7ac479e48e7fbb70b
jeromejaglale <jerome.jaglale@gmail.com>
2022-01-06 21:22:12 +0000

    Remove wrong comment

--- scripts/disable_https.sh
+++ scripts/disable_https.sh
@@ -2,7 +2,6 @@
 
 SCRIPT_DIR=`dirname "$0"`
 
-# install Docker
 echo "Disabling HTTPS.."
 sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service exec -T ireceptor-api \
 		sh -c 'a2dissite -q 000-default.conf && a2ensite -q http.conf && service apache2 reload'
--- scripts/enable_https.sh
+++ scripts/enable_https.sh
@@ -2,7 +2,6 @@
 
 SCRIPT_DIR=`dirname "$0"`
 
-# install Docker
 echo "Enabling HTTPS.."
 sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service exec -T ireceptor-api \
 		sh -c 'a2dissite -q http.conf && a2ensite -q 000-default.conf && service apache2 reload'
