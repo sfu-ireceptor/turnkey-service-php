9b438644154567f51cecbdf93c66d05a7a27af10
bcorrie <bdcorrie@gmail.com>
2022-04-04 21:52:12 +0000

    Turnkey v4 should use the turnkey-v4 Config file.

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,1 +1,1 @@
-MAPPING_URL=https://raw.githubusercontent.com/sfu-ireceptor/config/master/AIRR-iReceptorMapping.txt
+MAPPING_URL=https://raw.githubusercontent.com/sfu-ireceptor/config/turnkey-v4/AIRR-iReceptorMapping.txt
