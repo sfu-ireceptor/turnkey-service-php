8caff3c3ae678d89e304bbb0a0351ec4130db1f6
jeromejaglale <jerome.jaglale@gmail.com>
2022-10-03 21:21:04 +0000

    Update repository-mongodb scripts names (IR-2785)

--- scripts/restore_database.sh
+++ scripts/restore_database.sh
@@ -3,13 +3,13 @@
 SCRIPT_DIR=`dirname "$0"`
 
 sudo docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service exec -T ireceptor-database \
-	sh -c 'mongo --quiet /app/scripts/drop_indexes.js && \
+	sh -c 'mongo --quiet /app/scripts/drop_rearrangement_indexes.js && \
 			echo && \
 			echo "Restoring database..." && \
 			mongorestore --noIndexRestore --archive && \
 			echo "Done" && \
 			echo && \
-			cd /app && mongo --quiet /app/scripts/create_indexes.js && \
+			cd /app && mongo --quiet /app/scripts/create_rearrangement_indexes.js && \
 			cd /app && mongo --quiet /app/scripts/create_query_plans.js && \
 			echo && \
 			echo "Your database was sucessfully restored."'
