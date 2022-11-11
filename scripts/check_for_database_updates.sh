287c332403a6679b8f560ee52ee79b31e7b22bf0
jeromejaglale <jerome.jaglale@gmail.com>
2022-11-11 00:40:51 +0000

    Add update_dates.sh to check for updates script

--- scripts/check_for_database_updates.sh
+++ scripts/check_for_database_updates.sh
@@ -4,7 +4,15 @@ SCRIPT_DIR=`dirname "$0"`
 SCRIPT_DIR_FULL="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 DATABASE_NEEDS_TO_BE_UPDATED=0
+temp_file=$(mktemp)
 
-# TODO run database update check scripts
+if [ $DATABASE_NEEDS_TO_BE_UPDATED -eq 0 ]; then
+	${SCRIPT_DIR}/update_dates.sh sample '%a %b %d %Y %H:%M:%S %Z' check > temp_file
+	if [ `cat ${temp_file}`= "1" ]; then
+		DATABASE_NEEDS_TO_BE_UPDATED=1
+	fi
+fi
 
+rm ${temp_file}
 echo $DATABASE_NEEDS_TO_BE_UPDATED
+
