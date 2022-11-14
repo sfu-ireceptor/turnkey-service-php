369aebd5c7bbbfb9ca0708dd63f838b96c9a5d43
bcorrie <bdcorrie@gmail.com>
2022-11-14 23:02:21 +0000

    Fix to handle HTTP and HTTPS.

--- scripts/verify_dataload.sh
+++ scripts/verify_dataload.sh
@@ -5,15 +5,16 @@ SCRIPT_DIR=`dirname "$0"`
 # Get the command line arguements. Script assumes that study_dir is a directory
 # and that the directory contains both the $metadata_file as well as all of the
 # annotation files. The output from the command is written to $output_dir.
-if [ $# -eq 5 ]
+if [ $# -eq 6 ]
 then
     study_id="$1" 
     study_dir="$2"
     metadata_file="$3"
     annotation_tool="$4"
     output_dir="$5"
+    url="$6"
 else
-    echo "usage: $0 study_id study_dir metadata_file annotation_tool output_dir"
+    echo "usage: $0 study_id study_dir metadata_file annotation_tool output_dir url"
     exit
 fi
 
@@ -23,6 +24,7 @@ export study_dir
 export metadata_file
 export annotation_tool
 export output_dir
+export url
 
 # create log file
 LOG_FOLDER=${SCRIPT_DIR}/../log
@@ -49,6 +51,28 @@ if [ ! -f "$tmp_filename" ]; then
     exit 1
 fi
 
+STATUSCODE=$(curl --insecure --silent --output /dev/stderr --write-out "%{http_code}" ${url} 2> /dev/null)
+if [ ${STATUSCODE} -ne 200 ]
+then
+    echo "ERROR: Could not contact repository ${url}"
+    echo "HTTP Status = $STATUSCODE"
+    curl --insecure ${url}
+    exit 1
+fi
+
+# Replace localhost with the internal name within the docker environment.
+if [ ${url} = "http://localhost" ] || [ ${url} = "http://localhost/" ]
+then
+    url="http://ireceptor-api"
+fi
+
+# Replace localhost with the internal name within the docker environment.
+if [ ${url} = "https://localhost" ] || [ ${url} = "https://localhost/" ]
+then
+    url="https://ireceptor-api"
+fi
+
+
 # Tell the user what is going on.
 echo "Starting data verification at $TIME"
 echo "    Verifying study $study_id"
@@ -57,6 +81,7 @@ echo "    Metadata = $study_dir/$metadata_file"
 echo "    Annotations = $study_dir"
 echo "    Annotation tool = $annotation_tool"
 echo "    Output directory = $output_dir"
+echo "    Repository URL = $url"
 
 # -----------------------------------------------------------------------------------#
 # Notes:
@@ -66,13 +91,15 @@ echo "    Output directory = $output_dir"
 # docker-compose -v: mount VM volumes in the container. We mount $study_dir where all the input is and $output_dir where the output is written
 # "ireceptor-dataloading" is the service name defined in docker-compose.yml 
 # sh -c '...' is the command executed inside the container
+				
 sudo -E docker-compose --file ${SCRIPT_DIR}/docker-compose.yml --project-name turnkey-service run -v $study_dir:/study -v $output_dir:/output --rm \
 			-e study_id="$study_id" \
 			-e metadata_file="$metadata_file" \
 			-e annotation_tool="$annotation_tool" \
+			-e url="$url" \
 			ireceptor-dataloading \
 				sh -c 'bash /app/verify/joint_sanity_testing.sh \
-                                        http://ireceptor-api/ \
+                                        ${url} \
                                         "repertoire" \
                                         /app/verify/facet_queries_for_sanity_tests/ \
 					/app/verify/nofilters.json \
