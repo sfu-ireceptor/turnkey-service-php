af097c8a6536e96cbaaef0baee3ed84193e0a8c6
bcorrie <bdcorrie@gmail.com>
2024-10-03 16:32:19 +0000

    Make AIRR config file IPA4 specific

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,1 +1,1 @@
-MAPPING_URL=https://raw.githubusercontent.com/sfu-ireceptor/config/turnkey-v4/AIRR-iReceptorMapping.txt
+MAPPING_URL=https://raw.githubusercontent.com/sfu-ireceptor/config/turnkey-v4-ipa4/AIRR-iReceptorMapping.txt
