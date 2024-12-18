60e415c7b3fe49b3d73552df42ebe98e1cd5e928
bcorrie <bdcorrie@gmail.com>
2024-12-18 21:51:08 +0000

    Remove old code.

--- scripts/update_turnkey.sh
+++ scripts/update_turnkey.sh
@@ -26,38 +26,10 @@ echo
 
 
 # update AIRR mapping file
-#CONFIG_FOLDER="${SCRIPT_DIR}/../.config"
 echo "Updating AIRR-iReceptor mapping ${MAPPING_URL}"
 ${SCRIPT_DIR}/update_airr_config.sh
 echo "Done"
 echo ""
-
-#mkdir -p $CONFIG_FOLDER
-#sudo chmod 777 $CONFIG_FOLDER
-#curl -# -o $CONFIG_FOLDER/AIRR-iReceptorMapping_new.txt $MAPPING_URL
-#
-# if there wasn't any config file
-#if [ ! -f $CONFIG_FOLDER/AIRR-iReceptorMapping.txt ]; then
-#	cp $CONFIG_FOLDER/AIRR-iReceptorMapping_new.txt $CONFIG_FOLDER/AIRR-iReceptorMapping.txt
-#fi
-#
-# if the new config file is different
-#if [[ `diff --brief $CONFIG_FOLDER/AIRR-iReceptorMapping_new.txt $CONFIG_FOLDER/AIRR-iReceptorMapping.txt` != '' ]]
-#then
-#	CURRENT_DATETIME=`date +%Y-%m-%d_%H-%M-%S`
-#	ARCHIVED_NAME=AIRR-iReceptorMapping_${CURRENT_DATETIME}.txt
-#	mv $CONFIG_FOLDER/AIRR-iReceptorMapping.txt $CONFIG_FOLDER/${ARCHIVED_NAME}
-##	cp $CONFIG_FOLDER/AIRR-iReceptorMapping_new.txt $CONFIG_FOLDER/AIRR-iReceptorMapping.txt
-#	ARCHIVED_PATH=`realpath $CONFIG_FOLDER/${ARCHIVED_NAME}`
-#	echo "INFO: the mapping has been updated"
-#	echo "INFO: the previous mapping has been archived to $ARCHIVED_PATH"
-#fi
-
-#rm $CONFIG_FOLDER/AIRR-iReceptorMapping_new.txt
-#
-#echo "Done"
-#echo
-
 
 # generate and install self-signed SSL certificate for HTTPS
 SSL_FOLDER="${SCRIPT_DIR}/../.ssl"
